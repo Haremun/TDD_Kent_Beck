a1f6a2252010e8d3073246bd54fc560302ae50d9
Kamil <kamil_bieganski@epam.com>
2019-12-04 15:35:15 +0100

    added equals and hashCode and test for equals

--- src/main/java/com/bieganski/Dollar.java
+++ src/main/java/com/bieganski/Dollar.java
@@ -1,5 +1,7 @@
 package com.bieganski;
 
+import java.util.Objects;
+
 public class Dollar {
     int amount;
 
@@ -9,5 +11,18 @@ public class Dollar {
 
     Dollar times(int number) {
         return new Dollar(amount * number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
--- src/test/java/com/bieganski/DollarTest.java
+++ src/test/java/com/bieganski/DollarTest.java
@@ -13,4 +13,12 @@ public class DollarTest {
         product = five.times(5);
         Assert.assertEquals(25, product.amount);
     }
+    @Test
+    public void testEquality(){
+        Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
+    }
+    @Test
+    public void testEquality2(){
+        Assert.assertEquals(new Dollar(5), new Dollar(5));
+    }
 }
