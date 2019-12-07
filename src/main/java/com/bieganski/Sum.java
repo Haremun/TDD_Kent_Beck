30a9aa6f6027c158b811bd03833c7fd0577c1147
Kamil <kamilbieg96@gmail.com>
2019-12-07 16:25:10 +0100

    Chapter #16 Added method plus and times to Expression

--- src/main/java/com/bieganski/Sum.java
+++ src/main/java/com/bieganski/Sum.java
@@ -1,15 +1,26 @@
 package com.bieganski;
 
 public class Sum implements Expression {
-    Money augend; //Dodajna
-    Money addend; //Dodajnik
+    Expression augend; //Dodajna
+    Expression addend; //Dodajnik
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
-    public Money reduce(Bank bank, String to){
-        int amount = augend.amount + addend.amount;
+
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
     }
 }
